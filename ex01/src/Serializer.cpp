0d22b468dde1474465e6ed0e4e41db1833108bb0
Keillin-M <keillin.maeda@gmail.com>
2026-03-26 14:21:50 +0100

    add comment ex01

--- ex01/src/Serializer.cpp
+++ ex01/src/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: kmaeda <kmaeda@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 14:54:17 by kmaeda            #+#    #+#             */
-/*   Updated: 2026/01/31 17:03:31 by kmaeda           ###   ########.fr       */
+/*   Updated: 2026/03/26 14:20:53 by kmaeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,18 @@
 #include "../include/Data.hpp"
 
 Serializer::Serializer() {}
-Serializer::Serializer(const Serializer& other) {(void)other;}
+
+Serializer::Serializer(const Serializer& other) { (void)other; }
+
 Serializer& Serializer::operator=(const Serializer &other) {
 	(void)other;
 	return *this;
 }
+
 Serializer::~Serializer() {}
 
 uintptr_t Serializer::serialize(Data* ptr) {
-	return reinterpret_cast<uintptr_t>(ptr);
+	return reinterpret_cast<uintptr_t>(ptr); // bitwise reinterpretation
 }
 
 Data* Serializer::deserialize(uintptr_t raw) {
