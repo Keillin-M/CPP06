db9796fa299d9993def64236855e47ec264addf7
Keillin-M <keillin.maeda@gmail.com>
2026-03-26 13:43:15 +0100

    correct main with arg

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kmaeda <kmaeda@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 13:30:07 by kmaeda            #+#    #+#             */
-/*   Updated: 2026/01/31 12:41:39 by kmaeda           ###   ########.fr       */
+/*   Updated: 2026/03/26 13:41:57 by kmaeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,16 @@ int main(int argc, char **argv) {
 #include <string>
 #include "ScalarConverter.hpp"
 
-int main(void)
+int main (int argc, char **argv) {
+    if (argc != 2) {
+        std::cout << "Correct usage: ./convert + arg to be converted" << std::endl;
+        return 1;
+    }
+    ScalarConverter::convert(argv[1]);
+    return 0;
+}
+
+/*int main(void)
 {
     const std::string tests[] = {
         "a",
@@ -60,4 +69,4 @@ int main(void)
     }
 
     return 0;
-}
+}*/
